b86c5ae8ece7e4a894b54368c9bc178055c67a49
stilltay <stilltay@gmail.com>
2020-01-20 17:17:06 -0500

    Added firebase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "YOUR_CREDENTIALS_HERE",
-    authDomain: "YOUR_CREDENTIALS_HERE",
-    databaseURL: "YOUR_CREDENTIALS_HERE",
-    projectId: "YOUR_CREDENTIALS_HERE",
-    storageBucket: "YOUR_CREDENTIALS_HERE",
-    messagingSenderId: "YOUR_CREDENTIALS_HERE"
+    apiKey: "AIzaSyAtGSmXJEE8mCIzxBn-luSTUb4jbDM_MRU",
+    authDomain: "task-list-7cba3.firebaseapp.com",
+    databaseURL: "https://task-list-7cba3.firebaseio.com",
+    projectId: "task-list-7cba3",
+    storageBucket: "task-list-7cba3.appspot.com",
+    messagingSenderId: "305731952733"
   }
 };
 
