ccf1ce1e00cbe19e9532eecb2b8d4b1d9b1dc1db
Ajay Singh <singhajay08@gmail.com>
2020-01-20 19:05:20 -0500

    Added Firebase details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,24 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCNIAEGlWg0YuEVP7omETN-5EBMBRUdSKc",
+  authDomain: "tasks-820d7.firebaseapp.com",
+  databaseURL: "https://tasks-820d7.firebaseio.com",
+  projectId: "tasks-820d7",
+  storageBucket: "tasks-820d7.appspot.com",
+  messagingSenderId: "57109794410",
+  appId: "1:57109794410:web:481ee143f603c31d20c413",
+  measurementId: "G-SXBP295PXE"
+}
+
 
 @NgModule({
   declarations: [
